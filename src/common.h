6b90b92defcf16a511d0fbaee801889003dfac79
Andreas <i15084@student.htlwrn.ac.at>
2018-07-28 20:52:26 +0200

    format code

--- src/common.h
+++ src/common.h
@@ -1,13 +1,13 @@
 #ifndef COMMON_H
 #define COMMON_H
 
-typedef unsigned long  uint64_t;
-typedef          long  sint64_t;
-typedef unsigned int   uint32_t;
-typedef          int   sint32_t;
+typedef unsigned long uint64_t;
+typedef long sint64_t;
+typedef unsigned int uint32_t;
+typedef int sint32_t;
 typedef unsigned short uint16_t;
-typedef          short sint16_t;
-typedef unsigned char  uint8_t;
-typedef          char  sint8_t;
+typedef short sint16_t;
+typedef unsigned char uint8_t;
+typedef char sint8_t;
 
 #endif
--- src/console.c
+++ src/console.c
@@ -1,35 +1,35 @@
-#include "common.h"
 #include "console.h"
+#include "common.h"
 
 #define ROWS 25
 #define COLS 80
 
-uint8_t* video = (uint8_t*) 0xb8000;
+uint8_t* video = (uint8_t*)0xb8000;
 
 uint8_t x_screen = 0;
 uint8_t y_screen = 0;
 
 void k_clrscr() {
-    for(int i=0; i<ROWS*COLS*2; i++) {
+    for (int i = 0; i < ROWS * COLS * 2; i++) {
         video[i] = 0x0;
     }
 }
 
 void k_putc(uint8_t c, uint8_t attr) {
-    video[y_screen*COLS*2+x_screen*2] = c;
-    video[y_screen*COLS*2+x_screen*2+1] = attr;
+    video[y_screen * COLS * 2 + x_screen * 2] = c;
+    video[y_screen * COLS * 2 + x_screen * 2 + 1] = attr;
 
     x_screen++;
-    if(x_screen > 80) {
+    if (x_screen > 80) {
         x_screen = 0;
         y_screen++;
     }
 }
 
-void k_print(uint8_t *str, uint8_t attr) {
-   for (int i = 0;str[i] != '\0'; i++) {
-       k_putc(str[i], attr);
-   }
+void k_print(uint8_t* str, uint8_t attr) {
+    for (int i = 0; str[i] != '\0'; i++) {
+        k_putc(str[i], attr);
+    }
 }
 
 void k_printdec(uint32_t a, uint8_t attr) {
@@ -38,13 +38,13 @@ void k_printdec(uint32_t a, uint8_t attr) {
 
     int i = 11;
 
-    while(a >= 1) {
+    while (a >= 1) {
         uint8_t digit = a % 10;
-        buf[--i] = digit+48;
+        buf[--i] = digit + 48;
         a /= 10;
     }
 
-    k_print(buf+i, attr);
+    k_print(buf + i, attr);
 }
 
 void k_printhex(uint32_t a, uint8_t attr) {
@@ -55,12 +55,12 @@ void k_printhex(uint32_t a, uint8_t attr) {
 
     int i = 8;
 
-    while(a >= 1) {
+    while (a >= 1) {
         uint8_t digit = a % 16;
-        buf[--i] = digit < 10 ? digit+48 : digit+55;
+        buf[--i] = digit < 10 ? digit + 48 : digit + 55;
         a /= 16;
     }
 
     k_print(prefix, attr);
-    k_print(buf+i, attr);
+    k_print(buf + i, attr);
 }
